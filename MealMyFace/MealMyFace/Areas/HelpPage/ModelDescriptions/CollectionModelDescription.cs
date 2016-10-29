6799d1ccd91875cc2076a79d772c458039d36a49
mickeybnet <mickeybnet@gmail.com>
2016-10-29 13:50:25 -0500

    Initial Project creation

---
+++ MealMyFace/MealMyFace/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace MealMyFace.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ MealMyFace/MealMyFace/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace MealMyFace.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
